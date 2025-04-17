07a0c9f61ef85898a61a71ad3d2216fe7f0080e9
TarasLozinskyi <tlozinskyi@gmail.com>
2025-04-17 18:37:25 +0300

    completed task 1

---
+++ homework/hw2/task1/main.js
@@ -0,0 +1,17 @@
+// #67kfznmiMl
+//
+// – Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+const arr = [1, 'string', true, false, [], {}, null, undefined, 0, ''];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+console.log(arr.length) // довжина масиву
