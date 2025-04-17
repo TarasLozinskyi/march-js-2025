87c8d16135a2b3e55b86ad81190fc5b5330d40be
TarasLozinskyi <tlozinskyi@gmail.com>
2025-04-17 16:15:08 +0300

    completed the first and second tasks of homework 1

---
+++ homework/hw1/task1/main.js
@@ -0,0 +1,38 @@
+// #dYQNrBV
+
+// Створити змінні. Присвоїти кожному з них значення:
+// ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+let hello = 'hello';
+let owu = "owu";
+let com = "com";
+let ua = "ua";
+let number = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+const PI = 3.14; // це не тому що Сергій на уроці так написав а тому що 3.14 являєтся чилом PI а PI саме по собі не змінне, тобто константа.
+let number5 = 2.7;
+let number6 = 16;
+let bool = true;
+let bool2 = false;
+
+console.log(hello);    //hello
+console.log(owu);      //owu
+console.log(com);      //com
+console.log(ua);       //ua
+console.log(number);   //1
+console.log(number2);  //10
+console.log(number3);  //-999
+console.log(number4);  //123
+console.log(PI);       //3.14
+console.log(number5);  //2.7
+console.log(number6);  //16
+console.log(bool);     //true
+console.log(bool2);    //false
+
+
+
+
+
---
+++ homework/hw1/task2/main.js
@@ -0,0 +1,11 @@
+// #6Qb97gsv
+
+// – Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+
+let firstName = 'Тарас';
+let lastName = 'Лозинський';
+let middleName = 'Олексійович';
+
+let person = lastName + ' ' + firstName + ' ' + middleName;
+console.log(person);
