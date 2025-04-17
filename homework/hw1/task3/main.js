8ee5babb94ae290d7b498f10172c048a88f474bc
TarasLozinskyi <tlozinskyi@gmail.com>
2025-04-17 16:19:48 +0300

    completed the task3 of homework 1

---
+++ homework/hw1/task3/main.js
@@ -0,0 +1,13 @@
+// #4N0y5tufA
+//
+// – За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//  let a = 100; let b = ‘100’; let c = true;
+
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(a); //number
+console.log(b);  //string
+console.log(c);  //boolean
